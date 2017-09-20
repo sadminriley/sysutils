f63cf5a3d41498b4fa96a0926d93f90a8a909b82
Riley L <riley.louvier@homes.com>
2017-09-20 15:57:01 -0400

    Upload of csvparser.py

--- csvparser.py
+++ csvparser.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 import csv
+import sys
 
-csvdoc = raw_input('What CSV file would you like to parse?\n')
-delim = raw_input('What delimiter would you like to use while parsing?\n(examples would be , or .)\n')
-with open(csvdoc) as file:
-    reader = csv.reader(file, delimiter=delim, quotechar='|')
-    for item in reader:
-        print ' '.join(item)
+def main():
+    csvdoc = raw_input('What CSV file would you' +
+                       'like to parse?\n')
+    delim = raw_input('What delimiter would you like' +
+                      'to use while parsing?\n(examples would be , or .)\n')
+    try:
+        with open(csvdoc) as file:
+            reader = csv.reader(file, delimiter=delim, quotechar='|')
+            for item in reader:
+                print ' '.join(item)
+    except IOError as e:
+            print('Please enter a valid .csv file! Closing..')
+            sys.exit()
+
+if __name__ == '__main__':
+    main()
